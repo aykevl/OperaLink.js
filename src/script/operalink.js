1356a9de18f35dd652eea98e349c8059e998cc4f
Ayke van Laethem <aykevanlaethem@gmail.com>
2011-07-23 19:59:46 +0200

    Function added to drop the token
    This is useful when the tokens have been changed. When a request returns
    401 (unauthorized), the tokens are often outdated or revoked, and should
    be reloaded.

--- src/script/operalink.js
+++ src/script/operalink.js
@@ -433,6 +433,15 @@ opera.link = new function OperaLink() {
 		this.storage.removeItem('oauth_secret');
 	}
 
+	/**
+	 * Drops the OAuth token and token secret.
+	 */
+	this.dropToken = function() {
+		delete accessor.token;
+		delete accessor.tokenSecret;
+	}
+
+
 
 	/**
 	 * Parses the response of a token request
